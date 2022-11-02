b2baad909da75af5d1c192ac300c6293eb0d642a
irvinebes <anya.shmakova.96@bk.ru>
2022-11-03 00:12:27 +0300

    Выполненное третье задание во второй ветке

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        var dog = 8;
+        var dog = 8.0;
         System.out.println(dog);
         var cat = 3.6;
         System.out.println(cat);
@@ -11,6 +11,12 @@ public class Main {
         cat = cat + 4;
         System.out.println(cat);
         paper = paper + 4;
+        System.out.println(paper);
+        dog = dog - 3.5;
+        System.out.println(dog);
+        cat = cat - 1.6;
+        System.out.println(cat);
+        paper = paper - 7639;
         System.out.println(paper);
     }
 }
